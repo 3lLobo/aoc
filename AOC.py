4a02a7a819977bc426123ecad3e7f08b61b564f4
3lLobo <flocwolf@gmail.com>
2017-12-07 16:41:53 +0100

    final version

--- AOC.py
+++ AOC.py
@@ -1,43 +1,53 @@
 # AdventOfCode Day 7 - Florian Wolf
 
+class advent:
+	# Initialize class by creating empty result list and spliting up the input
+	def __init__(self,input):
+		self.keys =  {}
+		self.list = self.make_list(input)
+		
+	
+	# Funtion to slip up the input in left side, operator and right side of the equation
+	def make_list(self,input):
+		input1 = input.readlines()
 
-# Funtion to slip up the input in left side, operator and right side of the equation
-def make_list(input):
-	list = {}	
-	for line in input:
-		line.strip()
-		left, right = line.split('->')
-		list[right] = left.split()
-	return list
-	
-# Function to find the value of the key using the eoperators
-def inception(key,list,keys):
-	try:
-		return int(key)
-	except TypeError:
-	
-	if key is not in keys:
-		left = list(key)
-		if len(left) == 1:
-			solution = inception(left[0])
-		else:
-			if left[-2] == 'AND':
-				solution = inception(left[0]) & inception(left[-1])
-			if left[-2] == 'OR':
-				solution = inception(left[0]) | inception(left[-1])
-			if left[-2] == 'NOT':
-				solution = 65535 inception(left[1])
-			if left[-2] == 'LSHIFT':
-				solution = inception(left[0]) << inception(left[-1])
-			if left[-2] == 'RSHIFT':
-				solution = inception(left[0]) >> inception(left[-1])
-		keys [key] = solution
-	return keys(key)
+		list ={}
+		for line in input1:
+			(left, right) = line.split(' -> ')
+			
+			list[right.strip('\n')] = left.split()
+
+		return list
+		
+	# Function to find the value of the key using the eoperators
+	def inception(self,key):
+		try:
+			return int(key)
+		except ValueError:
+			pass
+		
+		if key not in self.keys:
+			inception = self.inception
+			left1 = self.list[key]
+			if len(left1) == 1:
+				solution = inception(left1[0])
+			else:
+				if left1[-2] == 'AND':
+					solution = inception(left1[0]) & inception(left1[-1])
+				if left1[-2] == 'OR':
+					solution = inception(left1[0]) | inception(left1[-1])
+				if left1[-2] == 'NOT':
+					solution = 65535 - inception(left1[1])
+				if left1[-2] == 'LSHIFT':
+					solution = inception(left1[0]) << inception(left1[-1])
+				if left1[-2] == 'RSHIFT':
+					solution = inception(left1[0]) >> inception(left1[-1])
+			self.keys[key] = solution
+		return self.keys[key] 
 	
 input = open('input.txt')
-list = make_list(input)
-keys = {}
-a = inception('a',list,keys)	
+aoc = advent(input)
+a = aoc.inception('a')	
 
 print ('Answer to the quiz: a = ',a)
 	
